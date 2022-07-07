1422f3733cf2cd114f1b04ce6241ea34d844d17a
bastien-riseandshine-agency <bastien@riseandshine.agency>
2022-07-07 15:39:59 +0200

    feat: update credit

--- resources/js/credit/credit.js
+++ resources/js/credit/credit.js
@@ -1,16 +1,24 @@
 const name = 'Bastien De Kan'
 
 console.log(
-    `%c ( ^ ) springbok Brussels`, 
+`%c^%c springbok Brussels %c
+Slicing made wide ♥ by ${name}`,
+    `   color:#000; 
+        font-family: Tahoma,Geneva,Verdana,sans-serif; 
+        font-weight: 600;
+        display: flex;
+        align-item: center;
+        justify-content: center;
+        padding: 0 3px;
+        line-height: 1.3;
+        border: 3px solid #000;
+        border-radius: 100px;`,
     `   color:#000; 
         font-family: Tahoma,Geneva,Verdana,sans-serif; 
         font-size: 14px; 
-        font-weight: 600;
-    `);  
-
-console.log(
-    `%c Slicing made wide ♥ by ${name}`, 
+        font-weight: 600;`,
     `   color:#000; 
         font-family: Tahoma,Geneva,Verdana,sans-serif; 
-        font-size: 14px;
-`);  
+        font-size: 14px;`);  
+
+ 
