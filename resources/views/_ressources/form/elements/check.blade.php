3983d9b99aae5cd9d6a44f3d9ea8bd7492584b27
bastien-riseandshine-agency <bastien@riseandshine.agency>
2022-07-07 15:40:07 +0200

    feat

--- resources/views/_ressources/form/elements/check.blade.php
+++ resources/views/_ressources/form/elements/check.blade.php
@@ -13,4 +13,4 @@
 <svg class="check-svg is-3" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
     <path class="check-svg-path" fill="none" stroke="#000" stroke-linejoin="bevel" stroke-width="2" d="M0,5.6093478 C3.08367111,10.1165319 4.75542886,11.3949825 5.11258845,11.9296428 C5.68424982,11.0609112 8.06574811,8.21451283 12.3231523,0" transform="translate(.791 1.363)"/>
 </svg>
-@endif
+@endif 
