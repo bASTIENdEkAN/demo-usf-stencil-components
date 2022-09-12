0ef1e5ad8cc4e55424c332ab178d5e7a2b66c812
bastien-riseandshine-agency <bastien@riseandshine.agency>
2022-09-12 15:52:24 +0200

    feat: hero-split

--- resources/views/pages/_resources.blade.php
+++ resources/views/pages/_resources.blade.php
@@ -4,10 +4,37 @@
 
 <progress class="header-progress progress" max="100" value="0" data-anim-to="75"> 70% </progress>
 
+<div class="hero-split">
+    <div class="hero-split__img-wrapper">
+        @include('_resources/elements/img',['path'=>'project-name_pictures_hero-split','ext'=>'jpg', 'class'=>'hero-split__img min-mobile'])
+        @include('_resources/elements/img',['path'=>'project-name_pictures_hero-split--mobile','ext'=>'jpg', 'class'=>'hero-split__img max-mobile'])
+    </div>
+    <div class="hero-split__content-wrapper">
+        <div class="hero-split__content-container">
+            <h1 class="hero-split__title title">
+                Starter Slicing Light
+            </h1>
+            <p class="subtitle">
+                Developed with love by Bastien De Kan
+            </p>
+            <div class="hero-split__content content">
+                <p>
+                    It is a set of component and javascript functionality made to facilitate the work of the slicer. 
+                    Take advantage of its resources or be inspired by them. The code is regularly updated.
+                </p>
+            </div>
+            <div class="hero-split__buttons">
+                <a href="#main_form" class="button">
+                    <span>Look at form components</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="wrapper">
-    <div class="container-small py-4"> 
-        <h1 class="title">Form components</h1>
-        <h2 class="subtitle">Developed with love by Bastien De Kan</h2>
+    <div class="container-small py-6" id="main_form"> 
+        <h2 class="title">Form components</h2>
         <div class="content">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, illo qui! Atque distinctio enim nihil, numquam, odit autem voluptatem corporis modi officia voluptate explicabo totam soluta. Dolorum veniam quibusdam consequatur.</p>
         </div>
