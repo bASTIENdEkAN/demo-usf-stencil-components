37309c547a803148c898a086044e8bfbdc134e21
Bastien De Kan <bastien.dekan@springbokagency.com>
2022-12-20 15:10:34 +0100

    fix: float label + test css grid

---
+++ resources/views/pages/_grid.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts/default')
+
+@section('content')
+
+<div class="wrapper">
+    <div class="container">
+        <div class="grid">
+            <div class="grid__el grid__el--a"></div>
+            <div class="grid__el grid__el--b"></div>
+            <div class="grid__el grid__el--c"></div>
+            <div class="grid__el grid__el--d"></div>
+            <div class="grid__el grid__el--e"></div>
+            <div class="grid__el grid__el--f"></div>
+            <div class="grid__el grid__el--g"></div>
+            <div class="grid__el grid__el--h"></div>
+            <div class="grid__el grid__el--i"></div>
+            <div class="grid__el grid__el--j"></div>
+            <div class="grid__el grid__el--k"></div>
+        </div>
+    </div>
+</div>
+    
+@endsection
